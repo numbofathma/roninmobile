dffeb0ac2adc0ef51f0c4b2b0b4bf12ca42fcd44
Costin Mirica <costin@roninmobile.eu>
2019-06-16 19:11:33 +0300

    Fix mobile detection.

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,19 +1,19 @@
 export const isMobile = (userAgent: string) => {
     return {
         isAndroid(): boolean {
-            return !!userAgent.match(/Android/i);
+            return userAgent.match(/Android/i) != null;
         },
         isBlackBerry(): boolean {
-            return !!userAgent.match(/BlackBerry/i);
+            return userAgent.match(/BlackBerry/i) != null;
         },
         isiOS(): boolean {
-            return !!userAgent.match(/iPhone|iPad|iPod/i);
+            return userAgent.match(/iPhone|iPad|iPod/i) != null;
         },
         isOpera(): boolean {
-            return !!userAgent.match(/Opera Mini/i);
+            return userAgent.match(/Opera Mini/i) != null;
         },
         isWindows(): boolean {
-            return !!userAgent.match(/IEMobile/i);
+            return userAgent.match(/IEMobile/i) != null;
         },
         isHandHeld(): boolean {
             return (this.isAndroid() || this.isBlackBerry() || this.isiOS() || this.isOpera() || this.isWindows());
