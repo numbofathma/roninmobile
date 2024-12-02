d6582d4e97d3e3fa27436bbfcbbc49570b08e8ac
Costin Mirica <costin.mirica@arnia.ro>
2024-12-02 22:54:58 +0200

    Remove attribute from cookieyes script tag

--- src/components/GoogleAnalytics/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics/GoogleAnalytics.tsx
@@ -5,7 +5,7 @@ import { COOKIE_YES_ID, GA_ID } from '@/constants';
 const GoogleAnalytics = () => {
   return (
     <>
-      <Script id='cookieyes' src={`https://cdn-cookieyes.com/client_data/${COOKIE_YES_ID}/script.js`} defer strategy='lazyOnload' />
+      <Script id='cookieyes' src={`https://cdn-cookieyes.com/client_data/${COOKIE_YES_ID}/script.js`} />
       <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} defer strategy='lazyOnload' />
       <Script id='google-analytics' defer strategy='lazyOnload'>
         {`
